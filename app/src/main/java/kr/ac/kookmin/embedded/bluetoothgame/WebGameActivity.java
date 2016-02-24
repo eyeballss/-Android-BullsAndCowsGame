a6c9006fa57aee0e2740da5b450800b78e79b3c7
eyeballs <kkvv1111@gmail.com>
2016-02-24 20:29:08 +0900

    16.2.24 Fixed bug from last commit

--- app/src/main/java/kr/ac/kookmin/embedded/bluetoothgame/WebGameActivity.java
+++ app/src/main/java/kr/ac/kookmin/embedded/bluetoothgame/WebGameActivity.java
@@ -1,10 +1,13 @@
 package kr.ac.kookmin.embedded.bluetoothgame;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +24,8 @@ public class WebGameActivity extends Activity {
     //채팅 리스트뷰
     ListView wChattingList;
     //유저 아이디
-    static String userId ="";
+    static String userId = "";
+    static String opponentId = "";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +44,13 @@ public class WebGameActivity extends Activity {
         final WConnectThread thrd = new WConnectThread(wChattingAdapter, ipAddr, portNum, this, userId);
         thrd.start();
 
+
+        /******************************
+         * 아래는 리스너들 (Send, ListViewItem)
+         * ******************************
+         */
+
+        //Send 버튼을 누르면 ....
         wSendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,8 +66,44 @@ public class WebGameActivity extends Activity {
                 }
             }
         });
+
+        wChattingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //첫번째 파라미터 : 클릭된 아이템을 보여주고 있는 AdapterView 객체(여기서는 ListView객체)
+            //두번째 파라미터 : 클릭된 아이템 뷰
+            //세번째 파라미터 : 클릭된 아이템의 위치(ListView이 첫번째 아이템(가장위쪽)부터 차례대로 0,1,2,3.....)
+            //네번재 파리미터 : 클릭된 아이템의 아이디(특별한 설정이 없다면 세번째 파라이터인 position과 같은 값)
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemStr = wChattingAdapter.getItem(position).toString();
+                opponentId = itemStr.substring(1, itemStr.indexOf("]")).toString().trim();
+
+                sendGameRequest(thrd);
+
+            }
+        });
+        //게임 요청을 보내는 메세지창
+
     }
 
+
+    public void sendGameRequest(final WConnectThread thrd) {
+        AlertDialog.Builder alert_confirm = new AlertDialog.Builder(this);
+        alert_confirm.setMessage("send game request to " + opponentId + " ?").setCancelable(false).setPositiveButton("YES",
+
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        thrd.write("game "+opponentId); //게임 요청 메세지를 보냄!
+                    }
+                }).setNegativeButton("NO",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alert_confirm.create();
+        alert.show();
+    }
 
 
 //    public void getUserId() {
